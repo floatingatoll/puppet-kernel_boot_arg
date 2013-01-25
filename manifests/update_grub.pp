cd585aae8aa39e7a37d3b06bf106946d876613d7
Richard Soderberg <rsoderberg@gmail.com>
2013-01-25 11:15:36 -0800

    fix lint complaint about quotes

--- manifests/update_grub.pp
+++ manifests/update_grub.pp
@@ -1,7 +1,7 @@
 class kernel_boot_arg::update_grub ($grub_file = hiera('kernel_boot_arg_debian_grub_file'), $config_file = hiera('kernel_boot_arg_debian_config_file')) {
     exec {
         'kernel_boot_arg_update-grub':
-            command   => "update-grub",
+            command   => 'update-grub',
             onlyif    => "[ ! -e ${grub_file} ] || [ ${grub_file} -ot ${config_file} ]",
             path      => '/usr/sbin:/sbin:/usr/bin:/bin';
     }
