41f69642f6703344969a2b96aa1a34b239e3e915
rsoderberg@mozilla.com <rsoderberg@mozilla.com>
2013-01-25 01:04:37 +0000

    fix path for update-grub

--- manifests/update_grub.pp
+++ manifests/update_grub.pp
@@ -1,8 +1,8 @@
-class kernel_boot_arg::update_grub ($path = hiera('kernel_boot_arg_path'), $grub_file = hiera('kernel_boot_arg_debian_grub_file'), $config_file = hiera('kernel_boot_arg_debian_config_file')) {
+class kernel_boot_arg::update_grub ($grub_file = hiera('kernel_boot_arg_debian_grub_file'), $config_file = hiera('kernel_boot_arg_debian_config_file')) {
     exec {
         'kernel_boot_arg_update-grub':
             command   => "update-grub",
             onlyif    => "[ ( ! -e ${debian_grub_file} ) -o ( ${debian_grub_file} -ot ${debian_config_file} ) ]",
-            path      => $path;
+            path      => '/usr/sbin:/sbin:/usr/bin:/bin';
     }
 }
