c98e2bb3afa7570390b5c04a963ecb780627bf19
floatingatoll <rsoderberg@gmail.com>
2013-01-27 17:35:18 -0800

    fix file paths in kernel_boot_arg::scripts

--- manifests/scripts.pp
+++ manifests/scripts.pp
@@ -5,13 +5,13 @@ class kernel_boot_arg::scripts ($path = hiera('kernel_boot_arg_path')) {
             owner  => root,
             group  => root,
             mode   => '0755',
-            source => 'puppet:///modules/kernel/boot_arg.pl';
+            source => 'puppet:///modules/kernel_boot_arg/boot_arg.pl';
         "${path}/kernel_boot_arg_modify.sh":
             ensure  => present,
             require => File["${path}/kernel_boot_arg.pl"],
             owner   => root,
             group   => root,
             mode    => '0755',
-            source  => 'puppet:///modules/kernel/boot_arg_modify.sh';
+            source  => 'puppet:///modules/kernel_boot_arg/boot_arg_modify.sh';
     }
 }
